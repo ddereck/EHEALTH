b6ddb2607154921bfa0b39592713e3f7466120ca
Gilles-Déreck GNANCADJA <itgillo60@gmail.com>
2023-08-02 15:17:21 +0200

    New changes

--- Front/vuehealth/src/views/pages/HomePage.vue
+++ Front/vuehealth/src/views/pages/HomePage.vue
@@ -8,7 +8,7 @@
           data-scrollax-parent="true">
           <div class="col-md-6 pt-5 ftco-animate">
             <div class="mt-5">
-              <span class="subheading">Welcome to Mediplus</span>
+              <span class="subheading">Welcome to EHEALTH</span>
               <h1 class="mb-4">We are here <br>for your Care</h1>
               <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
                 there live the blind texts. Separated they live in Bookmarksgrove.</p>
--- Front/vuehealth/src/views/pages/SignIn.vue
+++ Front/vuehealth/src/views/pages/SignIn.vue
@@ -1,34 +1,62 @@
 <template>
-    <div class="layer"></div>
+<div class="layer"></div>
 <main class="page-center">
   <article class="sign-up">
     <h1 class="sign-up__title">Welcome back!</h1>
     <p class="sign-up__subtitle">Sign in to your account to continue</p>
-    <form class="sign-up-form form" action="" method="">
-      <label class="form-label-wrapper">
+    <form class="sign-up-form form" @submit.prevent="handleSignIn">
+      <label class="form-label-wrapper" for="username">
         <p class="form-label">Email</p>
-        <input class="form-input" type="email" placeholder="Enter your email" required>
+        <input class="form-input" type="email" id="username" v-model="username" placeholder="Enter your email" required>
       </label>
       <label class="form-label-wrapper">
-        <p class="form-label">Password</p>
-        <input class="form-input" type="password" placeholder="Enter your password" required>
+        <p class="form-label" for="password">Password</p>
+        <input class="form-input" type="password" id="password" v-model="password" placeholder="Enter your password" required>
       </label>
       <a class="link-info forget-link" href="##">Forgot your password?</a>
       <label class="form-checkbox-wrapper">
         <input class="form-checkbox" type="checkbox" required>
         <span class="form-checkbox-label">Remember me next time</span>
       </label>
-      <button class="form-btn primary-default-btn transparent-btn">Sign in</button>
+      <div v-if="errorMessage" class="error-message">
+      {{ errorMessage }}
+      </div>
+      <button type="submit" class="form-btn primary-default-btn transparent-btn">Sign in</button>
     </form>
   </article>
 </main>
 </template>
   
   <script>
+  import axios from 'axios';
+
   export default {
     name: 'SignIn',
     // Logique du formulaire de connexion
+    // ...
+    methods: {
+    handleSignIn() {
+      const userData = {
+        username: this.username,
+        password: this.password
+      };
+
+      // Faire une requête POST vers l'API d'authentification
+      axios.post('http://127.0.0.1:8000/Backend/api_auth/login', userData)
+        .then(response => {
+          // Gérer la réponse de l'API après une connexion réussie
+          console.log('Utilisateur connecté !', response.data);
+          // Rediriger l'utilisateur vers une autre page si nécessaire
+          this.$router.push('/HomePage.vue');
+        })
+        .catch(error => {
+          // Gérer les erreurs d'authentification
+          console.error('Échec de connexion', error.response.data.message);
+          this.errorMessage = error.response.data.message;
+        });
+    }
   }
+};
   </script>
   
   <style scoped>
